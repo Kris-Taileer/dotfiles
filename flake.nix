ab78dcdf3c00b8403bea867165e76ee5f4548dc0
Kris Taileer <191796700+Kris-Taileer@users.noreply.github.com>
2026-07-05 11:10:33 +0300

    Add files via upload

--- flake.nix
+++ flake.nix
@@ -13,6 +13,7 @@
 			system = "x86_64-linux";
 			modules = [
 				./hosts/nixos-btw/configuration.nix
+				./modules/asus-numberpad-driver.nix
 				home-manager.nixosModules.home-manager
 				{
 					home-manager = {
