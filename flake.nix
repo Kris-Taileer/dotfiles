65455c758f793074679694c1f90193ec620335ae
Kris Taileer <191796700+Kris-Taileer@users.noreply.github.com>
2026-07-24 23:23:41 +0300

    Add files via upload

--- flake.nix
+++ flake.nix
@@ -7,20 +7,26 @@
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 		driftwm.url = "github:malbiruk/driftwm";
+		zen-browser = {
+			url = "github:0xc000022070/zen-browser-flake";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 	};
 
-	outputs = { self, nixpkgs, home-manager, driftwm, ... }: {
+	outputs = { self, nixpkgs, home-manager, driftwm, zen-browser, ... }@inputs: {
 		nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
 			system = "x86_64-linux";
 			modules = [
 				./hosts/nixos-btw/configuration.nix
 				./modules/asus-numberpad-driver.nix
+				./modules/minecraft-server.nix
 				driftwm.nixosModules.default
 				home-manager.nixosModules.home-manager
 				{
 					home-manager = {
 						useGlobalPkgs = true;
 						useUserPackages = true;
+						extraSpecialArgs = { inherit inputs; };
 						users.kris = import ./home/default.nix;
 						backupFileExtension = "backup";
 					};
